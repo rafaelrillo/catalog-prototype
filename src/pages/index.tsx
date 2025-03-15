b7a73755064a8bfa4427753138c5c4f909d8fb3d
Rafael Rillo <rafaelrillocabanne@gmail.com>
2025-03-15 20:34:52 -0300

    change the sample data base and added some images

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,7 +18,7 @@ const Home: React.FC = () => {
     { name: 'Tartas', id: 'category-2', cardBgColor: '#A6BFB1', portionBgColor: '#5A9272' },
     { name: 'Brownies', id: 'category-3',cardBgColor: '#F19C32', portionBgColor: '#C57512' },
     { name: 'Bocaditos', id: 'category-4',cardBgColor: '#C680CC', portionBgColor: '#AA17B6' },
-    { name: 'Servicios', id: 'category-5',cardBgColor: '#F8C8DC', portionBgColor: '#bc96c0' },
+    { name: 'Servicios', id: 'category-5',cardBgColor: '#E67792', portionBgColor: '#EB1951' },
   ];
 
   const sampleProducts = [
@@ -29,28 +29,28 @@ const Home: React.FC = () => {
       image: '/images/matilda.png',
     },
     {
-      name: 'Matilda',
-      description: 'Bizcochuelo de chocolate con dulce de leche, relleno y cubierto con crema bariloche',
+      name: 'Oreo',
+      description: 'Base de galletas Oreo con dulce de leche y crema chantilli x2',
       price: 9999,
-      image: '/images/matilda.png',
+      image: '/images/oreo.png',
     },
     {
-      name: 'Matilda',
-      description: 'Bizcochuelo de chocolate con dulce de leche, relleno y cubierto con crema bariloche',
+      name: 'Capita',
+      description: 'Capas crocantes intercaladas con dulce de leche cubierto con merengue',
       price: 9999,
-      image: '/images/matilda.png',
+      image: '/images/capita.png',
     },
     {
-      name: 'Matilda',
-      description: 'Bizcochuelo de chocolate con dulce de leche, relleno y cubierto con crema bariloche',
+      name: 'Nuez sin harina',
+      description: 'Base húmeda de nuez sin harinas con dulce de leche, crema y frutos rojos.',
       price: 9999,
-      image: '/images/matilda.png',
+      image: '/images/nuez-sin-harina.png',
     },
     {
-      name: 'Matilda',
-      description: 'Bizcochuelo de chocolate con dulce de leche, relleno y cubierto con crema bariloche',
+      name: 'Mousse Nutella',
+      description: 'Masa sable de cacao rellena con dulce de leche y mousse de nutella.',
       price: 9999,
-      image: '/images/matilda.png',
+      image: '/images/mouse-nutella.png',
     },
     
   ];
